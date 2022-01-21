0ec4f97d44852d13984b91c205a34251110ffa3c
gwbaik9717 <gwbaik9717@icloud.com>
2022-01-21 13:22:28 +0900

    3-4 문자열 활용하기

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
-const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 const content = document.createElement('div');
 const container = document.getElementById('root');
+const ajax = new XMLHttpRequest();
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
-let ajax = new XMLHttpRequest();
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
+function getData(url){
+    ajax.open('GET', url, false);
+    ajax.send();
+    return JSON.parse(ajax.response);
+}
 
-const newsFeed = JSON.parse(ajax.response);
+const newsFeed = getData(NEWS_URL);
 
 const ul = document.createElement('ul');
 
 window.addEventListener('hashchange', () => {
     const id = location.hash.substring(1);
-    ajax.open('GET', CONTENT_URL.replace('@id', id), false);
-    ajax.send();
-
-    
-    const newsContent = JSON.parse(ajax.response);
+    const newsContent = getData(CONTENT_URL.replace('@id', id));
     const title = document.createElement('h1');
     title.innerHTML = newsContent.title;
 
@@ -26,15 +25,15 @@ window.addEventListener('hashchange', () => {
 });
 
 for(let i=0; i < 10; i++){
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-
-    a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
-    a.href = `#${newsFeed[i].id}`;
-
+    const div = document.createElement('div');
     
-    li.appendChild(a);
-    ul.appendChild(li);
+    div.innerHTML = `
+    <li>
+      <a href="#${newsFeed[i].id}">${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
+    </li>
+    `;
+
+    ul.appendChild(div.firstElementChild);
 }
 
 
