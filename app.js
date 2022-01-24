a72ce9b192881843fc0bac9dd62204847fd6a278
gwbaik9717 <gwbaik9717@icloud.com>
2022-01-24 22:45:07 +0900

    3-6 페이징 구현하기

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+const store = {
+    currentPage: 1,
+}
 
 function getData(url){
     ajax.open('GET', url, false);
@@ -14,24 +17,33 @@ function newsFeed(){
     const newsList = [];
     newsList.push('<ul>');
     
-    for(let i=0; i < 10; i++){
+    for(let i=(store.currentPage - 1) * 10; i < store.currentPage*10; i++){
         newsList.push(`
         <li>
-          <a href="#${newsFeed[i].id}">${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
-        </li>`);
+          <a href="#/show/${newsFeed[i].id}">
+            ${newsFeed[i].title} (${newsFeed[i].comments_count})
+          </a>
+        </li>
+        `);
     }
     
     newsList.push('<ul>');
+    newsList.push(`
+      <div> 
+        <a href = "#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
+        <a href = "#/page/${store.currentPage < 3 ? store.currentPage + 1 : 3}">다음 페이지</a>
+      </div>
+    `)
     container.innerHTML = newsList.join('');
 }
 
 function newsDetail() {
-    const id = location.hash.substring(1);
+    const id = location.hash.substring(7);
     const newsContent = getData(CONTENT_URL.replace('@id', id));
 
     const template = `
     <h1>${newsContent.title}</h1>
-    <div><a href="#">목록으로</a></div>
+    <div><a href="#/page/${store.currentPage}">목록으로</a></div>
     `
     container.innerHTML = template;
 }
@@ -40,6 +52,9 @@ function newsDetail() {
 function router(){
     const routePath = location.hash;
     if(routePath === ''){
+        newsFeed();
+    }else if(routePath.indexOf('page') >= 0){
+        store.currentPage = Number(location.hash.substring(7));
         newsFeed();
     }else{
         newsDetail();
